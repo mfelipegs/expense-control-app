95acbda8f953629f3ad07ddcddf679a2982b72fc
mfelipegs <matheusg.dasilva7@gmail.com>
2023-01-02 20:13:13 -0300

    Feat: New useState w/button to update title prop and starting NewExpense component

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,23 +1,26 @@
-import React from 'react';
+import React, { useState } from "react";
 
 import ExpenseDate from "./ExpenseDate";
-import Card from '../UI/Card';
+import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  /*const expenseDate = new Date(2021, 2, 28);
-  const expenseTitle = 'Car Insurance';
-  const expenseAmount = 294.67;*/
+  const [title, setTitle] = useState(props.title);
+
+  const clickHandler = () => {
+    setTitle('Updated!');
+  };
 
   return (
     <Card className="expense-item">
-      <ExpenseDate date={props.date}/>
+      <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
