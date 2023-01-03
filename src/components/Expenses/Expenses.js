d7936dea624db1ea6e6f5464fa2c261fbb91d953
mfelipegs <matheusg.dasilva7@gmail.com>
2023-01-02 23:06:02 -0300

    Chore: Task 2 corrected

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import ExpenseItem from "./ExpenseItem";
 import ExpensesFilter from "./ExpensesFilter";
@@ -6,17 +6,15 @@ import Card from "../UI/Card";
 import "./Expenses.css";
 
 const Expenses = (props) => {
-  const pickExpenseYearHandler = (enteredYear) => {
-    const expenseData = {
-      ...enteredYear
-    };
-    props.onAddExpense(expenseData);
+    const [filteredYear, setFilteredYear] = useState('2020')
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
   };
 
   return (
     <div>
-      <ExpensesFilter onSaveExpenseYear={pickExpenseYearHandler}/>
       <Card className="expenses">
+        <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
         <ExpenseItem
           title={props.items[0].title}
           amount={props.items[0].amount}
